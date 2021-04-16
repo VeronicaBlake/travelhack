539713b3e5bc1f10d580e157068a3f788ad1f4d2
Veronica Blake/Noyce <vrex149@gmail.com>
2021-04-16 11:31:28 -0600

    user creation

--- client/app/AuthConfig.js
+++ client/app/AuthConfig.js
@@ -1,3 +1,3 @@
-export const domain = ''
-export const audience = ''
-export const clientId = ''
+export const domain = 'dev-k8azdd8c.us.auth0.com'
+export const audience = 'https://travelhackifier.com'
+export const clientId = 'fHJdemWFrTBkvpA43dW7RLhwjpu3RqxT'
